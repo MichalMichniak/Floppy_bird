650547713e508de17360b432bb3729d45ac8d242
MichalMichniak <michalmichniak2001@gmail.com>
2023-02-12 14:01:57 +0100

    nn control step

--- control.py
+++ control.py
@@ -13,7 +13,8 @@ class Connection:
         self.state = 0
     def __repr__(self):
         return f"{self.innovation_number}: ({self.input_neuron},{self.output_neuron}) w{self.weight},{'F' if self.is_feedforward else 'R'}"
-
+    def get_out(self):
+        return self.weight*self.state
 
 class Net:
     """
@@ -28,28 +29,73 @@ class Net:
         dict_of_input_conn : Dict[int,List[Connection]] = {i:[] for i in self.nr_inputs+self.neurons+self.nr_outputs}
         for i in range(len(self.connections)):
             dict_of_output_conn[self.connections[i].input_neuron].append(self.connections[i])
-            dict_of_input_conn
+            dict_of_input_conn[self.connections[i].output_neuron].append(self.connections[i])
         if activation_func == None:
             activation_func = lambda x: 1/(1+np.exp(-x))
         self.activation_func = activation_func
+        self.propagation_arrangement = self.nr_inputs.copy()
         ## detection of feedforward and recurent connections
+        """
+        sth like graph traversation but next edge is chosed among edges with least 
+        recurent connections so far
+        """
         visited = {i : False for i in self.nr_inputs+self.neurons+self.nr_outputs}
-        recurent = {i : False for i in self.nr_inputs+self.neurons+self.nr_outputs}
+        recurent = {i : 0 for i in self.nr_inputs+self.neurons+self.nr_outputs}
+        for co in self.connections:
+            recurent[co.output_neuron] +=1
         lst = []
         for i in self.nr_inputs:
-
+            visited[i] = True
             for j in range(len(dict_of_output_conn[i])):
                 dict_of_output_conn[i][j].is_feedforward = True
+                recurent[dict_of_output_conn[i][j].output_neuron] -= 1
+                if dict_of_output_conn[i][j].output_neuron not in lst:
+                    lst.append(dict_of_output_conn[i][j].output_neuron)
         
+        while lst != []:
+            recurent_count = [recurent[i] for i in lst]
+            idx = recurent_count.index(min(recurent_count))
+            temp = idx
+            idx = lst[idx]
+            lst.pop(temp)
+            visited[idx] = True
+            self.propagation_arrangement.append(idx)
+            for j in range(len(dict_of_output_conn[idx])):
+                dict_of_output_conn[idx][j].is_feedforward = True
+                recurent[dict_of_output_conn[idx][j].output_neuron] -= 1
+                if (dict_of_output_conn[idx][j].output_neuron not in lst) and (not visited[idx]):
+                    lst.append(dict_of_output_conn[idx][j].output_neuron)
+        self.dict_of_output_conn = dict_of_output_conn
+        self.dict_of_input_conn = dict_of_input_conn
 
-    def count(self):
-        pass
+
+    def count(self, input):
+        out = []
+        for in_neur_idx in range(len(self.nr_inputs)):
+            nuron_tag = self.nr_inputs[in_neur_idx]
+            for i in range(len(self.dict_of_output_conn[nuron_tag])):
+                self.dict_of_output_conn[nuron_tag][i].state = self.activation_func(input[in_neur_idx])
+        for current_neuron in self.propagation_arrangement[len(input):-len(self.nr_outputs)]:
+            ## biases here \/\/\/\/
+            signal = 0
+            for i in  range(len(self.dict_of_input_conn[current_neuron])):
+                signal += self.dict_of_input_conn[current_neuron][i].get_out()
+            for i in  range(len(self.dict_of_output_conn[current_neuron])):
+                self.dict_of_output_conn[current_neuron][i].state = self.activation_func(signal)
+            pass
+        for current_neuron in self.propagation_arrangement[-len(self.nr_outputs):]:
+            ## biases here \/\/\/\/
+            signal = 0
+            for i in  range(len(self.dict_of_input_conn[current_neuron])):
+                signal += self.dict_of_input_conn[current_neuron][i].get_out()
+            out.append(self.activation_func(signal))
+        return out
 
 if __name__ == '__main__':
 
     conn = [Connection(0,4,1,0),Connection(0,5,1,0),Connection(5,4,1,0),Connection(2,5,1,0)]
     net = Net([0,1,2,3],[4],[5],conn)
-    i = 0
+    print(net.count([0,0,0,0]))
     pass
 
 
@@ -59,7 +105,8 @@ class Control:
     def __init__(self,n_inputs, n_outputs) -> None:
         self.n_inputs = n_inputs
         self.n_outputs = n_outputs
-
+        self.conn = [Connection(0,4,1,0),Connection(0,5,1,0),Connection(5,4,1,0),Connection(2,5,1,0)]
+        self.net = Net([0,1,2,3],[4],[5],self.conn)
         pass
 
     def predict(self, input):
@@ -69,4 +116,4 @@ class Control:
             return 0
 
     def predict(self, input):
-        return 0
+        return self.net.count(input)[0] > 0.85
